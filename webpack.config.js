122b0a808966ed1b92654f88aba23c4230996b36
ewewezze <talkingtom32203@mail.ru>
2024-12-29 21:28:08 +0300

    - ML-02 add pug, config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const getAbsolutePath = (relativePath) => {
+  return path.resolve(__dirname, relativePath);
+};
 
 module.exports = {
   entry: {
@@ -11,6 +14,9 @@ module.exports = {
     clean: true,
     filename: "[name].js",
   },
+  resolve: {
+    extensions: [".js", ".pug"],
+  },
   module: {
     rules: [
       {
@@ -20,16 +26,18 @@ module.exports = {
       },
       {
         test: /\.pug$/,
-        loader: "pug-loader",
+        use: [
+          {
+            loader: "pug-loader",
+            options: {
+              pretty: true,
+            },
+          },
+        ],
       },
       {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
@@ -50,7 +58,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.pug"),
+      template: path.join(__dirname, "src", "main.pug"),
       filename: "index.html",
     }),
     new MiniCssExtractPlugin({
